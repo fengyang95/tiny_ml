7cd0c73c79042c6fa6224762a78b286d49a98c4e
wwdguu <git@github.com:wwdguu/wwdguu.github.io.git>
2018-10-20 23:49:51 +0800

    update lda

--- tinyml/discriminant_analysis/LDA.py
+++ tinyml/discriminant_analysis/LDA.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class LDA:
     def __init__(self):
-        self.omiga=None
+        self.omega=None
         self.omiga_mu_0=None
         self.omiga_mu_1=None
         pass
@@ -17,24 +17,24 @@ class LDA:
         X_1=X[np.where(y==1)]
         mu_0=np.mean(X_0,axis=0)
         mu_1=np.mean(X_1,axis=0)
-        S_omiga=X_0.T.dot(X_0)+X_1.T.dot(X_1)
-        invS_omiga=np.linalg.inv(S_omiga)
-        self.omiga=invS_omiga.dot(mu_0-mu_1)
-        self.omiga_mu_0=self.omiga.T.dot(mu_0)
-        self.omiga_mu_1=self.omiga.T.dot(mu_1)
+        S_omega=X_0.T.dot(X_0)+X_1.T.dot(X_1)
+        invS_omega=np.linalg.inv(S_omega)
+        self.omega=invS_omega.dot(mu_0 - mu_1)
+        self.omega_mu_0=self.omega.T.dot(mu_0)
+        self.omega_mu_1=self.omega.T.dot(mu_1)
         pass
 
     # 书上没讲怎么判断分类
     # 采用距离度量，计算X到两个投影中心的L2距离，分类为距离更近的类别。
     def predict_proba(self,X):
-        if self.omiga is None:
+        if self.omega is None:
             raise RuntimeError('cant predict before fit')
         n_samples = X.shape[0]
         extra = np.ones((n_samples,))
         X = np.c_[X, extra]
-        omiga_mu = X.dot(self.omiga)
-        d1=np.sqrt((omiga_mu-self.omiga_mu_1)**2)
-        d0=np.sqrt((omiga_mu-self.omiga_mu_0)**2)
+        omega_mu = X.dot(self.omega)
+        d1=np.sqrt((omega_mu-self.omega_mu_1)**2)
+        d0=np.sqrt((omega_mu-self.omega_mu_0)**2)
         prob_0=d1/(d0+d1)
         prob_1=1-prob_0
         return np.column_stack([prob_0, prob_1])
@@ -64,5 +64,3 @@ if __name__=='__main__':
     sklearn_pred=sklearn_lda.predict(X_test)
     print('sklearn prob:',sklearn_prob)
     print('sklearn pred:',sklearn_pred)
-
-from sklearn.tree import DecisionTreeClassifier
