c0efb3c9991660825845d27de860abd10ee7a8b1
fengyang <fengyang@fengyang.cloud.com>
2018-10-23 21:43:47 +0800

    udpate adaboost still some bugs

--- tinyml/ensemble/AdaBoostClassifier.py
+++ tinyml/ensemble/AdaBoostClassifier.py
@@ -5,9 +5,10 @@ import numpy as np
 import copy
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier as sklearnAdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import sklearn.datasets as datasets
 class AdaBoostClassifier:
-    def __init__(self, base_estimator=DecisionTreeClassifier(max_depth=1), n_estimators=300):
+    def __init__(self, base_estimator=LogisticRegression(solver='newton-cg'), n_estimators=300):
         self.base_estimator=base_estimator
         self.n_estimators=n_estimators
         self.hs_=[]
@@ -21,8 +22,11 @@ class AdaBoostClassifier:
         for t in range(self.n_estimators):
             ht=self.base_estimator
             ht.fit(X, y,self.Ds_[t])
-            y_pred = ht.predict(X)
-            epsilon_t =np.sum(self.Ds_[t][y!=y_pred])
+            y_pred = ht.predict(X).astype(np.int32)
+            valid_indices=(y!=y_pred)
+            mask=np.ones((len(y),))
+            mask[valid_indices]=0
+            epsilon_t =1-np.sum(self.Ds_[t]*mask)
             if epsilon_t>0.5:
                 break
             self.hs_.append(copy.copy(ht))
@@ -30,35 +34,49 @@ class AdaBoostClassifier:
             alpha_t=0.5*np.log((1-epsilon_t)/epsilon_t)
             self.alphas_.append(alpha_t)
             self.Ds_.append(self.Ds_[t]*np.exp(-alpha_t*y*y_pred))
-            self.Ds_[t+1]/=np.sum(self.Ds_[t+1])
+            self.Ds_[t + 1] = self.Ds_[t + 1] / np.sum(self.Ds_[t + 1])
+        self.alphas_=np.array(self.alphas_)
+        self.Ds_=np.array(self.Ds_)
+        self.epsilons_=np.array(self.epsilons_)
 
     @classmethod
     def calc_epsilon(clf,D,y_target,y_pred):
         return 1-np.sum(D[y_target==y_pred])
 
     def predict(self,X):
-        H=np.zeros((X.shape[0],))
-        for t in range(len(self.alphas_)):
-            H+=(self.alphas_[t]*self.hs_[t].predict(X))
-        print(self.alphas_)
-        return np.sign(H).astype(np.int32)
+        #H=np.zeros((X.shape[0],))
+        #for t in range(len(self.alphas_)):
+        #    print('pred:',self.hs_[t].predict(X))
+        #    H+=(self.alphas_[t]*self.hs_[t].predict(X))
+        #return np.sign(H)
+        self.classes_=np.array([-1,1])
+        classes = self.classes_[:, np.newaxis]
+        pred = None
+
+        pred = sum((estimator.predict(X) == classes).T * w
+                       for estimator, w in zip(self.hs_,
+                                               2*self.alphas_))
+        pred /= self.alphas_.sum()
+        pred[:, 0] *= -1
+        pred=pred.sum(axis=1)
+        return self.classes_.take(pred > 0, axis=0)
 
 
 if __name__=='__main__':
 
-    X,y=datasets.make_classification(n_samples=200,n_features=5,n_classes=2)
+    breast_data=datasets.load_breast_cancer()
+    X,y=breast_data.data,breast_data.target
     y=2*y-1
+    X_train,y_train=X[:200],y[:200]
+    X_test,y_test=X[200:],y[200:]
 
-    X_train,y_train=X[:150],y[:150]
-    X_test,y_test=X[150:],y[150:]
-
-    sklearn_decision_tree=DecisionTreeClassifier(max_depth=1)
+    sklearn_decision_tree=LogisticRegression(solver='newton-cg')
     sklearn_decision_tree.fit(X_train,y_train)
     y_pred_decison_tree=sklearn_decision_tree.predict(X_test)
     print('single decision tree:',len(y_test[y_pred_decison_tree==y_test])*1.0/len(y_test))
 
     print('tinyml:')
-    adaboost_clf = AdaBoostClassifier(n_estimators=100,base_estimator=DecisionTreeClassifier(max_depth=1))
+    adaboost_clf = AdaBoostClassifier(n_estimators=10)
     adaboost_clf.fit(X_train,y_train)
     print('alpha:',adaboost_clf.alphas_)
     print('epsilon:',adaboost_clf.epsilons_)
@@ -66,7 +84,7 @@ if __name__=='__main__':
     print('adaboost y_pred:', len(y_test[y_pred==y_test])*1./len(y))
 
     print('sklearn:')
-    sklearn_adboost_clf=sklearnAdaBoostClassifier(n_estimators=100,base_estimator=DecisionTreeClassifier(max_depth=1))
+    sklearn_adboost_clf=sklearnAdaBoostClassifier(n_estimators=100,random_state=False,algorithm='SAMME',base_estimator=LogisticRegression(solver='newton-cg'))
     sklearn_adboost_clf.fit(X_train,y_train)
     print('alpha:',sklearn_adboost_clf.estimator_weights_)
     print('epsilon:',sklearn_adboost_clf.estimator_errors_)
