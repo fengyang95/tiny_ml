74358bece32c492059154c6c48a34f1baec9566e
wwdguu <git@github.com:wwdguu/wwdguu.github.io.git>
2018-11-09 10:14:01 +0800

    fix LogisticRegressor

--- tinyml/linear_model/LogisticRegression.py
+++ tinyml/linear_model/LogisticRegression.py
@@ -24,33 +24,21 @@ class LogisticRegression:
 
     @staticmethod
     def _dldbeta(X,y,beta):
-        '''
         # 《机器学习》 公式 3.30
-        # 分步计算的方法
         m=X.shape[0]
         sum=np.zeros(X.shape[1],).T
         for i in range(m):
-            sum+=(y[i]-np.exp(X[i].dot(beta))/(1+np.exp(X[i].dot(beta))))
-        '''
-        # vectorize的方法
-        sum=np.sum(y-np.exp(X.dot(beta))/(1+np.exp(X.dot(beta))),axis=0)
+            sum+=X[i]*(y[i]-np.exp(X[i].dot(beta))/(1+np.exp(X[i].dot(beta))))
         return -sum
 
     @staticmethod
     def _dldldbetadbeta(X,beta):
-        '''
         # 《机器学习》公式 3.31
-        # 非向量化的方法
         m=X.shape[0]
         sum=0.
         for i in range(m):
             p1=np.exp(X[i].dot(beta))/(1+np.exp(X[i].dot(beta)))
             sum+=X[i].dot(X[i].T)*p1*(1-p1)
-
-        '''
-        # 向量化的方法
-        p1=np.exp(X.dot(beta))/(1+np.exp(X.dot(beta)))
-        sum=np.sum(X.dot(X.T)*p1*(1-p1))
         return sum
 
     def predict_proba(self,X):
@@ -74,12 +62,12 @@ if __name__=='__main__':
     X, y = breast_data.data[:,:2], breast_data.target
     X = MinMaxScaler().fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    tinyml_logisticreg = LogisticRegression(max_iter=1000)
+    tinyml_logisticreg = LogisticRegression(max_iter=100)
     tinyml_logisticreg.fit(X_train, y_train)
     lda_prob = tinyml_logisticreg.predict_proba(X_test)
     lda_pred = tinyml_logisticreg.predict(X_test)
     # print('tinyml logistic_prob:', lda_prob)
-    # print('tinyml logistic_pred:', lda_pred)
+    print('tinyml logistic_pred:', lda_pred)
     print('tinyml accuracy:', len(y_test[y_test == lda_pred]) * 1. / len(y_test))
 
     sklearn_logsticreg = linear_model.LogisticRegression(max_iter=100,solver='newton-cg')
